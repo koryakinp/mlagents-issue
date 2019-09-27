fff66d1dee2c167f9e99b75122fe4a16d83d25dc
koryakinp <koryakinp@koryakinp.com>
2019-09-26 20:54:48 -0400

    -minor fix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from mlagents.envs import UnityEnvironment
 
-env = UnityEnvironment(
-  file_name="environments/mldriver-discrete-steering", worker_id=1)
+env = UnityEnvironment(file_name="mldriver-discrete-steering", worker_id=1)
 
 brain_name = env.brain_names[0]
 
